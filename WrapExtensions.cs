035e8c87be966c25a35438d0865d51b290a98a5d
Scott Thomas <kidfearless1@gmail.com>
2024-01-21 20:35:06 -0700

    Add project files.

---
+++ WrapExtensions.cs
@@ -0,0 +1,18 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+using Newtonsoft.Json;
+
+public static class WrapExtensions
+{
+  public static async Task<T?> ExecuteJsonAsync<T>(this Command command)
+  {
+    var result = await command.ExecuteBufferedAsync();
+    var output = result.StandardOutput;
+    var returnValue = JsonConvert.DeserializeObject<T>(output);
+    return returnValue;
+  }
+}
+
+
+
